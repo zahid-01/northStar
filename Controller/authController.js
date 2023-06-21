ede14a7855a79c0b2a2934e8109ad220c75e2103
zahid-01 <erzahidkhan101@gmail.com>
2023-06-21 20:43:18 +0530

    Beta V1-03

--- Controller/authController.js
+++ Controller/authController.js
@@ -28,10 +28,10 @@ const createSendToken = ({ _id }, res, req) => {
 
   const cookieOptions = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
+    // httpOnly: true,
     sameSite: "none",
-    // secure: req.secure || req.headers["x-forwarded-proto"] === "https",
-    secure: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+    // secure: true,
   };
 
   res.cookie("JWT", cookie, cookieOptions);
